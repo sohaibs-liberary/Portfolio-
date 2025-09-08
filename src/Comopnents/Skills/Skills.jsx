adbc08a84a7f0dfea6a7ab740fa3da4e1c3d1cf3
sohaibs-liberary <muhammadsohaibshakeel8gmail.com>
2025-09-08 20:17:31 +0500

    make the responsive website

--- src/Comopnents/Skills/Skills.jsx
+++ src/Comopnents/Skills/Skills.jsx
@@ -82,19 +82,23 @@ const Skills = () => {
                 <h1 className='text-gray-600 font-bold text-xl text-center' >A collection of my technical skills and expertise honed through various projects and experiences</h1>
             </div>
         </div>
-        <div className="skill">
-            <div className="skills flex  justify-center gap-5 py-8">
+        <div className="skill ">
+            <div className="skills  lg:flex  justify-center md:mx-20 gap-5 py-8">
+
                 {/* FONTEND */}
-                <div className="first max-w-[550px] max-h-[600px] border-white border-[1px] drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)] rounded-lg py-10 px-20">
+                <div className="skills lg:flex  justify-center md:mx-20 gap-5 py-8">
+
+                
+                <div className="first shadow-[0_10px_20px_rgba(130,69,236,0.5)]  max-w-[590px] max-h-[600px] border-white border-[1px] rounded-lg py-10 px-20">
                    <h1 className='text-4xl text-white font-extrabold font-sans text-center' >Frontend</h1>
                   
-                   <ul className="grid grid-cols-3 gap-8 my-8" >
+                   <ul className="grid lg:grid-cols-3 sm:grid-cols-2  md:grip-cols-2 lg:gap-8  gap-5 my-8  " >
 
                     {
                         Frontend.map((data,index)=>(
                             <li key={index} >
 
-                            <div className="flex justify-center  min-w-32  max-h-9 p-1 rounded-full border-gray-600 border-[1px] ">
+                            <div className="flex justify-center  lg:min-w-32  sm:w-36 max-h-9 p-1 rounded-full border-gray-600 border-[1px] ">
                             <img src={data.img} alt="language img"  className='text-white max-w-[40px] max-h-[30px]  ' />
                             <h1 className='  text-gray-400' >{data.name}</h1>
                          </div>   
@@ -106,17 +110,17 @@ const Skills = () => {
 
                 </div>
                 {/* TOOLS */}
-                 <div className="first max-w-[550px] max-h-[600px] border-white border-[1px] drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)] rounded-lg py-10 px-20">
+                 <div className="first max-w-[590px] max-h-[690px] border-white border-[1px] lg:my-0 my-16  shadow-[0_10px_20px_rgba(130,69,236,0.5)] rounded-lg py-10 px-20">
                    <h1 className='text-4xl text-white font-extrabold font-sans text-center' >Skills</h1>
                   
-                   <ul className="grid grid-cols-3 gap-5 my-8 " >
+                   <ul className="grid lg:grid-cols-3 sm:grid-cols-2  md:grip-cols-2 lg:gap-8 gap-5 my-8  " >
 
                     {
                         Skills.map((data,index)=>(
                             <li key={index} >
 
-                            <div className="flex justify-center  min-w-32  max-h-9 p-1 rounded-full border-gray-600 border-[1px] ">
-                            <img src={data.img} alt="language img"  className='text-white max-w-[40px] max-h-[30px]  ' />
+                            <div className="flex justify-center gap-2 min-w-32  max-h-9 p-1 rounded-full border-gray-600 border-[1px] ">
+                            <img src={data.img} alt="language img"  className='text-white max-w-[30px] max-h-[30px]  ' />
                             <h1 className='  text-gray-400' >{data.name}</h1>
                          </div>   
                             </li>
@@ -125,6 +129,7 @@ const Skills = () => {
                    
                     </ul>
 
+                </div>
                 </div>
             </div>
 
