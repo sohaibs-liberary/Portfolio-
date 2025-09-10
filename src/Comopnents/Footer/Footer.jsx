98fd95b9177679358501d87d0dcfec6533d3da8a
sohaibs-liberary <muhammadsohaibshakeel8gmail.com>
2025-09-10 14:56:40 +0500

    full website ki completed and esponsive website

--- src/Comopnents/Footer/Footer.jsx
+++ src/Comopnents/Footer/Footer.jsx
@@ -1,20 +1,66 @@
 import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+
+
+
+
 
 const Footer = () => {
-    const Menu =[
-        {
-            
-        },
-    ];
+    const mainmenu = [
+    { id: "about", Lable: "About" },
+    { id: "skills", Lable: "Skills" },
+    { id: "projects", Lable: "Projects" },
+    { id: "contact", Lable: "Contact" },
+  ];
   return (
     <>
     <div className="footer ">
-        <div className="grid justify-center bg-slate-950 text-center  grid-cols-1 gap-2">
+        <div className="grid justify-center bg-slate-950 text-center  grid-cols-1 gap-6">
         <div className="name text-2xl font-bold bg-gradient-to-r from-purple-800 to-pink-600 bg-clip-text text-transparent">Sohaib Shakeel</div>
 
-        <div className="menu"></div>
-        <div className="sociamediaaps"></div>
-        <div className="copywright"></div>
+        <div className="menu">
+              <div className="hidden md:block lg:block">
+            <ul className="flex justify-center lg:gap-9 gap-3 font-semibold">
+              {mainmenu.map((data) => (
+                <li
+                  key={data.id}
+                  className="text-center text-white"
+                >
+                  <button onClick={() => handleMenuItemClick(data.id)}>
+                    {data.Lable}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="sociamediaaps flex justify-center gap-6 ">
+          <a href="">
+          <FaFacebook  className='text-white text-2xl hover:text-purple-800 items-center ' />
+          </a>
+           <a href="">
+          <FaTwitter  className='text-white text-2xl hover:text-purple-700 items-center ' />
+          </a>
+           <a href="">
+          <FaLinkedin
+           className='text-white text-2xl hover:text-blue-700 items-center ' />
+          </a>
+           <a href="">
+          <FaInstagram  className='text-white text-2xl hover:text-red-700 items-center ' />
+          </a>
+          <a href="">
+          <FaGithub  className='text-white text-2xl hover:text-gray-700  items-center ' />
+          </a>
+          
+        </div>
+        <div className="copywright">
+          <h1 className='text-gray-600 text-xl' >© 2025 Sohaib Shakeel. All rights reserved.</h1>
+        </div>
         </div>
     </div>
     </>
